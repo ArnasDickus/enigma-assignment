4b1e44bacd65a2a9ef22dad7874a67d094317633
Arnas <1arnasdickus1@gmail.com>
2019-02-16 17:57:57 +0200

    navbar responsive

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -7,38 +7,36 @@ class CustomNavbar extends Component {
   render() {
     return (
       
-        <Navbar default collapseOnSelect>
-          <Container>
-            <Navbar.Brand href = "#home">Enigma Assignment</Navbar.Brand>
-              <Navbar.Toggle />
-              <Navbar.Collapse className="justify-content-end">
-                    <Nav>
-                      <Nav.Item >
-                        <LinkContainer  to = "./">
-                        <Nav.Link eventkey = {1}   >Home</Nav.Link>
-                        </LinkContainer>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <LinkContainer to = "./Register" >
-                        <Nav.Link eventkey = {2}    >Register</Nav.Link> 
-                        </LinkContainer> 
-                      </Nav.Item>
-                      <Nav.Item>
-                        <LinkContainer to = "./Login">
-                        <Nav.Link eventkey = {3}    >Login</Nav.Link> 
-                        </LinkContainer>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <LinkContainer to = "./Display">
-                        <Nav.Link eventkey = {4}    >Display</Nav.Link> 
-                        </LinkContainer>
-                      </Nav.Item>
-                    </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        
-   
+      <Navbar expand = "lg">
+        <Container>
+          <Navbar.Brand href = "./">Enigma Assignment</Navbar.Brand>
+            <Navbar.Toggle  aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                  <Nav >
+                    <Nav.Item >
+                      <LinkContainer  to = "./">
+                      <Nav.Link eventkey = {1}   >Home</Nav.Link>
+                      </LinkContainer>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <LinkContainer to = "./Register" >
+                      <Nav.Link eventkey = {2}    >Register</Nav.Link> 
+                      </LinkContainer> 
+                    </Nav.Item>
+                    <Nav.Item>
+                      <LinkContainer to = "./Login">
+                      <Nav.Link eventkey = {3}    >Login</Nav.Link> 
+                      </LinkContainer>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <LinkContainer to = "./Display">
+                      <Nav.Link eventkey = {4}    >Display</Nav.Link> 
+                      </LinkContainer>
+                    </Nav.Item>
+                  </Nav>
+            </Navbar.Collapse>
+          </Container>
+      </Navbar>
     )
   }
 }
