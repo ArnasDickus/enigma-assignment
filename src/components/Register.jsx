dd4b13c26c243bf0e30fd03d95473c2d08ea560e
Arnas <1arnasdickus1@gmail.com>
2019-02-19 13:20:48 +0200

    Shorten code for inputs

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,53 +8,42 @@ class Register extends Component {
     this.state = {
       email: '',
       password: '',
+      data: {
+        email: '',
+        password: '',
+      },
     }
 
+/*
+  Take inputs Done
+  Put inputs in local storage and allow to push it into it.
+*/
     // This binding is necessary to make `this` work in the callback 
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.storeData = this.storeData.bind(this);
+
   }
 
-  handleEmailChange = (event) => {
-    this.setState({email: event.target.value});
-  }
-  handlePasswordChange = (event) => {
-    this.setState({password: event.target.value});
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
-  storeData = () => {
 
-    if(localStorage.getItem("data") === null){
-        let dataArray = [];
-        this.setState(prevState => ({
-          data: [...prevState.data, dataArray]
-        }))
-        localStorage.setItem('data', JSON.stringify(dataArray));
-    }else{
-        // let data = JSON.parse(localStorage.getItem("data"));
-        // data.push(bookmarkObject);
-        // localStorage.setItem("data", JSON.stringify(dataArray));
-    }
-  }
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state.email);
-    console.log(this.state.password);
-    let data = "";
-    localStorage.setItem('data', JSON.stringify(data));
-    /*
-    Get values from inputs Done
-    Take values from input, ant put it into this state data array
-
-    */
     this.storeData();
-
+    console.log(this.state);
+  // Reset form;
     this.setState({
       email: '',
       password: '',
     })
+  }
+
+  storeData = () => {
+    localStorage.setItem('data', JSON.stringify(this.state.data));
   }
 
 
@@ -65,7 +54,13 @@ class Register extends Component {
         <Form >
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-             <Form.Control value = {this.state.email} onChange = {this.handleEmailChange} type="email" placeholder="Enter email" /> 
+             {/* <Form.Control value = {this.state.email} onChange = {this.handleEmailChange} type="email" placeholder="Enter email" />  */}
+             <Form.Control 
+             name = "email"
+             value = {this.state.email} 
+             onChange = {event => this.onChange(event)} 
+             type="email" 
+             placeholder="Enter email" />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -73,7 +68,13 @@ class Register extends Component {
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-             <Form.Control value = {this.state.password} onChange = {this.handlePasswordChange} type="password" placeholder="Password" /> 
+             {/* <Form.Control value = {this.state.password} onChange = {this.handlePasswordChange} type="password" placeholder="Password" />  */}
+             <Form.Control 
+             name = "password"
+             value = {this.state.password} 
+             onChange = {event => this.onChange(event)} 
+             type="password" 
+             placeholder="Password" /> 
           </Form.Group>
           <Button onClick = {this.handleSubmit} variant="primary" type="submit">
             Submit
