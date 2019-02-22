e7fa4e8b29e67d48c864eee36d10de598ac20433
Arnas <1arnasdickus1@gmail.com>
2019-02-22 19:38:48 +0200

    added some text to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,15 @@ class Home extends Component {
 render() {
     return (
         <Container className = "mt-3">
-           This is home page
+           <h2>Home</h2>
+           <p>Front-end project 4 page application created with React.</p>
+           <ul className = "list-group">
+               <li className = "list-group-item"><strong>Register:</strong> Allows you to register stores info into local storage</li>
+               <li className = "list-group-item"><strong>Login:</strong> Checks if you email and password is registered </li>
+               <li className = "list-group-item"><strong>Display:</strong> Display all items, also allows to edit and delete items</li>
+                <li className = "list-group-item"><strong>Bonus:</strong> Added login validation. won't register empty users, and won't allow to have the same email</li>
+           </ul>
+           <a className="badge badge-primary mt-2" href="https://github.com/ArnasDickus/enigma-assignment" target = "_blank" rel = 'noopener noreferrer'>Github</a>
         </Container>
         )
     }
