018fb8f813b431fe914102843a267f4f63c47192
Arnas Dickus <arnasdickus@protonmail.com>
2020-09-24 23:11:47 +0300

    Updates

---
+++ precache-manifest.a64caddb3e8e44a5f79f2e4cbac890a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb3aaa54bf3579bd6f95",
+    "url": "/enigma-assignment/static/js/runtime~main.eb3aaa54.js"
+  },
+  {
+    "revision": "f6ed6d5a492d11174a4b",
+    "url": "/enigma-assignment/static/js/main.f6ed6d5a.chunk.js"
+  },
+  {
+    "revision": "fb80159618858075a82e",
+    "url": "/enigma-assignment/static/js/2.fb801596.chunk.js"
+  },
+  {
+    "revision": "f6ed6d5a492d11174a4b",
+    "url": "/enigma-assignment/static/css/main.3b640a65.chunk.css"
+  },
+  {
+    "revision": "a4fc3b5e3d31b7d1acf4000ebeceef34",
+    "url": "/enigma-assignment/index.html"
+  }
+];
